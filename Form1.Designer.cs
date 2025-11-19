30ab23fbd067b7043a1cc1ee3835318c23aab486
marron-mobo <mizu200688@gmail.com>
2025-11-19 22:54:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace 車掌用知らせ灯
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label1.Location = new System.Drawing.Point(81, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 14);
+            this.label1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(147, 153);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 16);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "ワンマン車両のみ再生";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(147, 175);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 16);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "停止";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label2.Location = new System.Drawing.Point(81, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 14);
+            this.label2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 21);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "車種判定更新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 223);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "発車放送";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using WMPLib;
+using TrainCrew;
+
+namespace 車掌用知らせ灯
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer; // UI更新用タイマー
+        private StringBuilder sb = new StringBuilder(); // 状態表示用テキスト
+        private int IsAvail = 0; // 音声ファイルが利用可能かどうか
+        private int IsPlayed = 0; // 発車放送が再生されたか
+        private int IsStopped = 0; // 停止状態のフラグ
+        private string sound; // 音声ファイルのパス
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosing += Form1_FormClosing;
+
+            // 初期化処理
+            TrainCrewInput.Init();
+
+            // タイマー設定
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+            timer.Tick += CircleDraw;
+            timer.Interval = 200;
+            timer.Start();
+
+            // 音声ファイルの確認
+            IsAudioFileAvail();
+
+            // チェックボックスの初期状態設定
+            checkBox1.Checked = true;
+        }
+
+        // タイマーのTickイベント
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var state = TrainCrewInput.GetTrainState();
+
+            sb.Clear();
+            sb.AppendLine("速度: " + state.Speed.ToString("0.0km/h"));
+            sb.AppendLine("戸閉: " + state.AllClose);
+            sb.AppendLine(state.nextStaName + " " + state.nextStopType);
+            sb.AppendLine("残り: " + state.nextStaDistance + "m");
+            sb.AppendLine("制限: " + state.speedLimit + "km/h");
+        }
+
+        // フォームが閉じられるときの処理
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TrainCrewInput.Dispose();
+        }
+
+        // 発車放送再生の処理
+        private void CircleDraw(object sender, EventArgs e)
+        {
+            var state = TrainCrewInput.GetTrainState();
+
+            if (state.AllClose && IsPlayed == 0 && IsStopped == 0 && IsAvail == 1 && !string.IsNullOrEmpty(sound))
+            {
+                var player = new WindowsMediaPlayer
+                {
+                    URL = sound
+                };
+                player.controls.play();
+                IsPlayed = 1;
+            }
+            else if (!state.AllClose)
+            {
+                IsPlayed = 0; // 戸閉が解除されたらリセット
+            }
+        }
+
+        // 音声ファイルの存在確認
+        private void IsAudioFileAvail()
+        {
+            try
+            {
+                string soundFilePath = Path.Combine(Application.StartupPath, "departure.wav");
+                if (File.Exists(soundFilePath))
+                {
+                    sound = soundFilePath;
+                    label1.Text = "発車放送が見つかりました";
+                    IsAvail = 1;
+                }
+                else
+                {
+                    label1.Text = "発車放送が見つかりません";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "エラー: " + ex.Message;
+            }
+        }
+
+        // 停止状態の切り替え
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            IsStopped = checkBox2.Checked ? 1 : 0;
+        }
+
+        // 車種判定とUI更新
+        private void Hantei()
+        {
+            if (checkBox1.Checked)
+            {
+                try
+                {
+                    var state = TrainCrewInput.GetTrainState();
+                    string carmodel = state.CarStates[0].CarModel;
+
+                    if (carmodel == "3300V" || carmodel == "5600" || carmodel == "4600")
+                    {
+                        IsStopped = 0;
+                        label2.Text = "ワンマン車両";
+                    }
+                    else
+                    {
+                        IsStopped = 1;
+                        label2.Text = "ワンマン車両でない";
+                    }
+                }
+                catch
+                {
+                    label2.Text = "車種判定不能";
+                }
+            }
+            else
+            {
+                IsStopped = 0;
+                label2.Text = "チェックボックスが押下されていない";
+            }
+        }
+
+        // チェックボックス変更時の処理
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Hantei();
+        }
+
+        // ボタン押下時の処理
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hantei();
+        }
+    }
+}
